a0387acbf9a03cf26709b21af4f19d15bad8326d
OscarArcia <oarcia99@gmail.com>
2019-02-06 16:22:26 -0600

    Updates

---
+++ precache-manifest.8e0b1c778a3312c64820e954cac6b988.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "feffafe6dc2fde283b9e",
+    "url": "/kygoReact/static/js/runtime~main.feffafe6.js"
+  },
+  {
+    "revision": "ca1c73061bd53e88509b",
+    "url": "/kygoReact/static/js/main.ca1c7306.chunk.js"
+  },
+  {
+    "revision": "e6d028e3339aa9b72943",
+    "url": "/kygoReact/static/js/1.e6d028e3.chunk.js"
+  },
+  {
+    "revision": "ca1c73061bd53e88509b",
+    "url": "/kygoReact/static/css/main.aa6ac5f9.chunk.css"
+  },
+  {
+    "revision": "2898f108fb5dbdd24d3f2d677b303a3a",
+    "url": "/kygoReact/index.html"
+  }
+];
